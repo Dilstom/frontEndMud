f5ef68e5db32f1d8c867944d57e8a5c7dacaeee7
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-12-10 06:51:19 -0800

    new components

---
+++ front_mud/src/components/Adventure.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Adventure extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
+}
+
+export default Adventure;
---
+++ front_mud/src/components/RegistrationPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class RegistrationPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>RP</div>
+        );
+    }
+}
+
+export default RegistrationPage;
